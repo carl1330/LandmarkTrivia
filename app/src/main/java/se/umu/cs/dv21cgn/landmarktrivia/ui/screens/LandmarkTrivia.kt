df0d109b19ce5b04e169dca266fb90f2c80c726d
Carl Gulliksson <gulliksson.carl@gmail.com>
2023-05-27 18:34:35 +0200

    fix: After location access has been granted TriviaCards are now automatically updated

--- app/src/main/java/se/umu/cs/dv21cgn/landmarktrivia/ui/screens/LandmarkTrivia.kt
+++ app/src/main/java/se/umu/cs/dv21cgn/landmarktrivia/ui/screens/LandmarkTrivia.kt
@@ -5,11 +5,9 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,21 +18,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -61,15 +54,12 @@ class LandmarkTrivia : ComponentActivity() {
             val scope = CoroutineScope(Dispatchers.IO)
             val (cards, setCards) = remember { mutableStateOf(LocationResult()) }
             val showNoLocationDialog = remember { mutableStateOf(false) }
-            val locationLauncher = rememberLauncherForActivityResult(
-                ActivityResultContracts.RequestPermission()
-            ) { isGranted ->
-
-            }
+            var locationGranted = false
             val context = this
 
+
             @Suppress("DEPRECATION")
-            fun getUserLocation(context: Context, requestPermissionLauncher: ActivityResultLauncher<String>, fusedLocationProviderClient: FusedLocationProviderClient) {
+            fun getUserLocation(context: Context, fusedLocationProviderClient: FusedLocationProviderClient) {
                 val geocoder = Geocoder(context)
                 when {
                     ContextCompat.checkSelfPermission(
@@ -101,11 +91,14 @@ class LandmarkTrivia : ComponentActivity() {
                     shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) -> {
                         showNoLocationDialog.value = true
                     }
-                    else -> {
-                        requestPermissionLauncher.launch(
-                            Manifest.permission.ACCESS_COARSE_LOCATION)
-                    }
                 }
+            }
+
+            val locationLauncher = rememberLauncherForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted ->
+                locationGranted = isGranted
+                getUserLocation(context, fusedLocationClient)
             }
 
             LandmarkTriviaTheme {
@@ -132,7 +125,9 @@ class LandmarkTrivia : ComponentActivity() {
                         }
                         CitySearchBar(setCards,
                             onGetLocationClick = {
-                                getUserLocation(context, locationLauncher, fusedLocationClient)
+                                if(!locationGranted)
+                                    locationLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                                getUserLocation(context, fusedLocationClient)
                             })
                         LazyColumn(
                             modifier = Modifier.padding(0.dp, 16.dp),
